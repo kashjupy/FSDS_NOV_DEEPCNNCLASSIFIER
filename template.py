c4b6d41b5463b317355b2435b3f81b6c67a6087f
kashjupy <karthikhvk963@gmail.com>
2023-03-25 22:01:57 +0530

    typing template.py

---
+++ template.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO,format='%(asctime)s]:%(message)s')
+project_name="deepClassifier"
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"scrc/{package_name}/components/_init_.py",
+    f"scrc/{package_name}/components/_init_.py",
+    f"scrc/{package_name}/components/_init_.py",
+    f"scrc/{package_name}/components/_init_.py",
+    f"scrc/{package_name}/components/_init_.py",
+    f"scrc/{package_name}/components/_init_.py",
+    
+
+]
